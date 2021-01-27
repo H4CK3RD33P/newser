afcce605cdd0119e642dcb3a84f835def100d68c
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-01-27 23:27:53 +0530

    Created 'Article' Django model class.
    An Article object will have title, author's name, description, body, creation time and updation time as attributes.
    Creation time is permanent, and it will be set when the object is created.
    Updated time will change whenever any attribute of the Article object is changed and saved.

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return(f'{self.title} by {self.author}')

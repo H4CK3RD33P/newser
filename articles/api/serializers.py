c21bc175aca31c5b61b93bf32d0322c43fb72013
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-01-28 00:16:02 +0530

    Created ArticleSerializer class
    Created a folder api/ under articles/ and inside it, created serializers.py.
    It contains the ArticleSerializer for Article model. It can create and update model model instances.

---
+++ articles/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from articles.models import Article
+
+class ArticleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField()
+    author = serializers.CharField()
+    description = serializers.CharField()
+    body = serializers.CharField()
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
+
+    def create(self,validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title',instance.title)
+        instance.author = validated_data.get('author',instance.author)
+        instance.description = validated_data.get('description',instance.description)
+        instance.body = validated_data.get('body',instance.body)
+        instance.save()
+        return instance
+

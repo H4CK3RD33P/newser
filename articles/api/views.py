54f08a44f58ee569e2667ceb8e5f6154ffe241b3
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-01-28 19:48:23 +0530

    Added detail_article() view
    detail_article() accepts primary key, and it can fetch and view a single article when it gets a GET request.
    It can update a article upon getting a PUT request.
    It can also delete upon getting a DELETE request.

--- articles/api/views.py
+++ articles/api/views.py
@@ -17,3 +17,27 @@ def all_articles(request):
             return Response(serialzer.validated_data, status.HTTP_201_CREATED)
         else:
             return Response(serialzer.validated_data, status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def detail_article(request,pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return Response({"error":{
+            "code":404,
+            "message": "Article not found!"
+        }}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data,status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        

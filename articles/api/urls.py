eeccfde2e9834a2728cb8889057455ac6fd35170
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-01-30 15:25:41 +0530

    Added path api/all/<primary-key>
    All the requests to this path will be handled by detail_article() which will let the user to view, update or delele a particular article.

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import all_articles
+from .views import detail_article
 urlpatterns = [
     path('all/',all_articles,name='all_articles'),
+    path('all/<int:pk>/',detail_article,name='detail_article'),
 ]

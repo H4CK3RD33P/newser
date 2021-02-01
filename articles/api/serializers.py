0f1036a1e111180eca50de36671618f260a7193e
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-02-01 19:20:57 +0530

    Inherited serializer.ModelSerializer in ArticleSerializer
    serializer.ModelSerializer is DRF's built in class which can create
    serializer classes that corresponds to a Django model class, by just creating
    a Meta inner class with two attributes. 1) model = ModelName 2) exclude/fields = fields-you-don't-want-to-show/fields-that-you-want-to-show.

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -1,22 +1,7 @@
 from rest_framework import serializers
 from articles.models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    author = serializers.CharField()
-    description = serializers.CharField()
-    body = serializers.CharField()
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
-
-    def create(self,validated_data):
-        return Article.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.author = validated_data.get('author',instance.author)
-        instance.description = validated_data.get('description',instance.description)
-        instance.body = validated_data.get('body',instance.body)
-        instance.save()
-        return instance
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        exclude = ('id',)

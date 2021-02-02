bcd4c1abc0ce01559634d103901e0fbaac804e91
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-02-03 00:04:44 +0530

    Altered 'author' field in Article model class
    'author' attribute is now a foreign key that refers to the corresponding 'Author' object

--- articles/models.py
+++ articles/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Article(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return(f'{self.title} by {self.author}')
-
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -19,3 +8,14 @@ class Author(models.Model):
 
     def __str__(self):
         return(f'{self.first_name} {self.last_name}')
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    description = models.TextField(max_length=300)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return(f'{self.title} by {self.author}')

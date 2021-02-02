6ba66d12431716111c73a4bdee99497b993b2521
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-02-02 22:12:08 +0530

    Created AuthorSerialzer class in serializers.py
    AuthorSerializer represents Author Django model class

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from articles.models import Article
+from articles.models import Article, Author
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
+        exclude = ('id',)
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
         exclude = ('id',)

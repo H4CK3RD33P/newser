14dc1984acc0363e91d389c3895b5bf76e2e4aef
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-02-02 22:46:24 +0530

    Created all_authors and detail_author path
    All the requests to api/authors will be handled by all_authors() view
    and all requests to api/authors/primary_key will be handled by detail_author() view

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import all_articles
-from .views import detail_article
+from .views import *
 urlpatterns = [
     path('all/',all_articles,name='all_articles'),
     path('all/<int:pk>/',detail_article,name='detail_article'),
+    path('authors',all_authors,name='all_authors'),
+    path('authors/<int:pk>/',detail_author,name='detail_author'),
 ]

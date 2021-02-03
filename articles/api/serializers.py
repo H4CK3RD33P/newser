ec5ccc41be846da5f531a6a3c5d2002ef0b5f222
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-02-03 14:32:25 +0530

    Added field level validators in ArticleSerializer & AuthorSerializer
    ArticleSerializer --> title must be more than or equal to 5 characters
    AuthorSerializer  --> country name (nationality) must be more than or equal to 4 characters

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -10,8 +10,13 @@ class ArticleSerializer(serializers.ModelSerializer):
     def validate(self,data):
         if data['title']==data['description']:
             raise serializers.ValidationError("Title and description must be different")
-        else:
-            return data
+        return data
+
+    def validate_title(self,value):
+        if len(value)<2:
+            raise serializers.ValidationError("Title too short!")
+        return value
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     articles = serializers.HyperlinkedRelatedField(read_only=True,many=True,view_name="detail_article")
@@ -22,5 +27,9 @@ class AuthorSerializer(serializers.ModelSerializer):
     def validate(self,data):
         if data['first_name']==data['last_name']:
             raise serializers.ValidationError("First and last name must be different")
-        else:
-            return data
+        return data
+
+    def validate_nationality(self,value):
+        if len(value)<4:
+            raise serializers.ValidationError("No country has this small name!")
+        return value
